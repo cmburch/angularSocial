93623efea559f735557333127efb40e22598f1f6
Charles Burch <georgia_beast89@yahoo.com>
2020-02-22 12:25:40 -0500

    fixed the inner join statement for customer and orders

--- sql_store_database_queries/sql_store_customer_orders_join_where_matching_ids.sql
+++ sql_store_database_queries/sql_store_customer_orders_join_where_matching_ids.sql
@@ -1,4 +1,3 @@
 SELECT order_id,first_name,last_name,orders.customer_id
 From Orders
-Join Customers
-Where orders.customer_id = customers.customer_id
+Join Customers On orders.customer_id = customers.customer_id

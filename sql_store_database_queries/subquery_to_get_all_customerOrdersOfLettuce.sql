82ceb7bbe9ebe96da70cf98421823400fb77c36f
Charles Burch <georgia_beast89@yahoo.com>
2020-02-24 00:44:57 -0500

    join tables to get all customers orders of lettuce

--- sql_store_database_queries/subquery_to_get_all_customerOrdersOfLettuce.sql
+++ sql_store_database_queries/subquery_to_get_all_customerOrdersOfLettuce.sql
@@ -2,7 +2,24 @@ SELECT * FROM sql_store.order_items;
 
 -- used subquery to get a list of orders  for lettuce Note:product_id = 3
 
-SELECT o.customer_id ,oi.product_id
+SELECT
+	customer_id,
+    first_name,
+    last_name
+From customers
+Where customer_id In
+(SELECT o.customer_id 
 FROM order_items oi
 Join orders o USING (order_id)
-where product_id = 3;
+where product_id = 3);
+
+-- using joins to get a list of orders for lettuce Note:product_id = 3
+Select 
+	Distinct customer_id,
+    first_name,
+    last_name
+From customers c
+Join orders o USING(customer_id)
+Join order_items oi USING(order_id)
+WHERE product_id = 3;
+

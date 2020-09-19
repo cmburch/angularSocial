8aec35e6fe88808cea178d5fab7775f4977967d8
Charles Burch <charles_burch89@yahoo.com>
2020-09-19 16:32:08 -0400

    aggregate queries exercise completed

--- Postgres_ZTM/employees_simple_queries.sql
+++ Postgres_ZTM/employees_simple_queries.sql
@@ -14,8 +14,61 @@ SELECT count(emp_no) FROM employees;
 SELECT Max(salary) FROM salaries;
 
 -- Get the total amount of salaries paid
-SELECT Sum(salary) FROM salaries;
 
+
+
+
+--*****************************************Aggregate 
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+-- 
+
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
+
+SELECT Avg(salary) FROM salaries;
+
+-- Question 2: What year was the youngest person born in the company?
+-- Table: employee
+SELECT min(birth_date) AS "BirthDate" FROM employees;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+-- 
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+SELECT count(id) FROM France;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+-- 
+
+-- Question 1: How many official languages are there?
+-- Table: countrylanguage
+SELECT count(countrycode) FROM countrylanguage
+WHERE isofficial = TRUE;
+
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+SELECT Avg(lifeexpectancy) AS "life expectancy" FROM country;
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+SELECT AVG(population) FROM city
+WHERE countrycode = 'NLD';
 
 
 

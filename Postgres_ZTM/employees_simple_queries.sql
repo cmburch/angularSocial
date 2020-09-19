2fef255ad6b09c6984921c84d0bbbdcd8352215b
Charles Burch <charles_burch89@yahoo.com>
2020-09-19 12:34:10 -0400

    postgres concat fullname for employee query

---
+++ Postgres_ZTM/employees_simple_queries.sql
@@ -0,0 +1,8 @@
+-- SELECT emp_no As "Employee #", birth_date AS "Birthday"
+-- FROM employees
+-- 
+-- Select * 
+-- From employees
+
+SELECT emp_no, Concat(first_name, ' ', last_name) AS "Full Name"
+FROM employees

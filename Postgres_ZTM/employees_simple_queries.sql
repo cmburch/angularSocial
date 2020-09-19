9707ab8b9bbff4e8305c0a8c9e9f4e160dccf0f4
Charles Burch <charles_burch89@yahoo.com>
2020-09-19 14:17:01 -0400

    add notes for aggregate and scalar functions

--- Postgres_ZTM/employees_simple_queries.sql
+++ Postgres_ZTM/employees_simple_queries.sql
@@ -6,3 +6,9 @@
 
 SELECT emp_no, Concat(first_name, ' ', last_name) AS "Full Name"
 FROM employees
+
+
+-- //Notes 
+-- //2 types of functions are AGGREGATE and scalar FUNCTIONS
+-- // AGGREGATE functions combine a row of data and return single output
+-- // Scalar FUNCTIONS runs a function for each individual row and return multiple output

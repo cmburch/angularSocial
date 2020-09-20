8302f8f9de2249e32b0491f3989bb107cbaf76d2
Charles Burch <charles_burch89@yahoo.com>
2020-09-20 08:10:49 -0400

    ILIKE and LIKE operator examples

--- Postgres_ZTM/employees_simple_queries.sql
+++ Postgres_ZTM/employees_simple_queries.sql
@@ -110,6 +110,56 @@ SELECT avg(COALESCE(age, 15)) FROM "Student";
 */
 SELECT id, COALESCE(NAME, 'fallback'), COALESCE(NAME, 'lastName'), age FROM "Student";
 
+--*************Like operator
+/*
+* DB: Employees
+* Table: employees
+* Question: Find the age of all employees who's name starts with M.
+* Sample output: https://imgur.com/vXs4093
+* Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
+*/
+SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) AS "age" FROM employees;
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many people's name start with A and end with R?
+* Expected output: 1846
+*/
+SELECT count(emp_no) FROM employees 
+WHERE first_name ILIKE 'A%R';
+
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode have a 2 in it?.
+* Expected output: 4211 
+*/
+SELECT count(zip) AS "ZIP" FROM customers
+WHERE zip::TEXT ILIKE '%2%'
+
+
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+* Expected output: 109 
+*/
+SELECT count(zip) FROM customers
+WHERE zip::TEXT ILIKE '2_1%';
+
+/*
+* DB: Store
+* Table: customers
+* Question: Which states have phone numbers starting with 302?
+* Replace null values with "No State"                                                  
+* Expected output: https://imgur.com/AVe6G4c
+*/
+SELECT COALESCE(state,'No State') AS "State",phone FROM customers 
+WHERE phone::TEXT ILIKE '302%';
+
+
 -- //Notes 
 -- //2 types of functions are AGGREGATE and scalar FUNCTIONS
 -- // AGGREGATE functions combine a row of data and return single output

dcfef5140c6882cdbb80b958061bf887dfebd472
Charles Burch <charles_burch89@yahoo.com>
2020-09-20 03:09:53 -0400

    coalesce operator example

--- Postgres_ZTM/employees_simple_queries.sql
+++ Postgres_ZTM/employees_simple_queries.sql
@@ -93,6 +93,23 @@ WHERE (age >= 30 AND age <= 50) AND income < 50000;
 SELECT AVG(income) FROM customers
 WHERE age >= 20 AND age <= 50;
 
+--*********************Coalesce
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Assuming a students minimum age for the class is 15, what is the average age of a student?
+*
+* 30.4000000000000000
+*/
+SELECT avg(COALESCE(age, 15)) FROM "Student";
+
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Replace all empty first or last names with a default?
+*/
+SELECT id, COALESCE(NAME, 'fallback'), COALESCE(NAME, 'lastName'), age FROM "Student";
+
 -- //Notes 
 -- //2 types of functions are AGGREGATE and scalar FUNCTIONS
 -- // AGGREGATE functions combine a row of data and return single output
